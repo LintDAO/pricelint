b1fd4d473b44efd36f3aa016e21b20ef7e33559b
prj6 <prj6@qq.com>
2025-06-04 21:41:14 +0800

    Add api of query named users

--- backend/src/web/api/user_api.rs
+++ backend/src/web/api/user_api.rs
@@ -6,7 +6,7 @@ use crate::{impl_storable, map_get, map_insert, USER_CONTEXT};
 use candid::{CandidType, Principal};
 use ic_cdk::{call, caller, query, update};
 use crate::web::services::user_service::{ExtendUserService, UserService};
-
+use crate::web::common::guard::{is_named_user,is_admin};
 #[query]
 async fn user_login() -> Result<bool, String> {
     if caller() == Principal::anonymous() {
@@ -21,4 +21,15 @@ async fn user_login() -> Result<bool, String> {
 #[update]
 fn create_user(){
     User::create_deafult_user();
+}
+
+#[query(guard = "is_admin")]
+fn find_user_lists()->Vec<User>{
+    let option_users=User::find_all();
+    match option_users{
+        None => {vec![]}
+        Some(vec_users) => {
+            vec_users
+        }
+    }
 }
