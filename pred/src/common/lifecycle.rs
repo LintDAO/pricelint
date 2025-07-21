b4851e7fb93ca8b3461f8ace184af5331b28a9ae
prj6 <prj6@qq.com>
2025-07-21 17:05:59 +0800

    Integrate canistergeek_ic_rust into project

--- pred/src/common/lifecycle.rs
+++ pred/src/common/lifecycle.rs
@@ -1,4 +1,4 @@
-use ic_cdk::init;
+use ic_cdk::{call, init, post_upgrade, pre_upgrade};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::storable::Bound;
 use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap, Storable};
@@ -6,6 +6,8 @@ use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
+use candid::Principal;
+use ic_cdk::api::call::CallResult;
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 
@@ -18,6 +20,10 @@ thread_local! {
     ));
          pub static CONFIG: RefCell<StableBTreeMap<String, Value<String>, Memory>> = RefCell::new(StableBTreeMap::init(
         MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+    ));
+
+         pub static N: RefCell<StableBTreeMap<String, Value<String>, Memory>> = RefCell::new(StableBTreeMap::init(
+        MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2)))
     ));
 }
 
@@ -51,9 +57,31 @@ unsafe extern "Rust" fn __getrandom_v03_custom(
     dest: *mut u8,
     len: usize,
 ) -> Result<(), getrandom::Error> {
-    //TODO  complete it
+    //TODO:  complete it
     Ok(())
 }
 
 #[init]
 fn init() {}
+#[pre_upgrade]
+fn pre_upgrade_function() {
+    let monitor_stable_data = canistergeek_ic_rust::monitor::pre_upgrade_stable_data();
+    let logger_stable_data = canistergeek_ic_rust::logger::pre_upgrade_stable_data();
+    ic_cdk::storage::stable_save((monitor_stable_data, logger_stable_data));
+}
+
+#[post_upgrade]
+fn post_upgrade_function() {
+    let stable_data: Result<(canistergeek_ic_rust::monitor::PostUpgradeStableData, canistergeek_ic_rust::logger::PostUpgradeStableData), String> = ic_cdk::storage::stable_restore();
+    match stable_data {
+        Ok((monitor_stable_data, logger_stable_data)) => {
+            canistergeek_ic_rust::monitor::post_upgrade_stable_data(monitor_stable_data);
+            canistergeek_ic_rust::logger::post_upgrade_stable_data(logger_stable_data);
+        }
+        Err(e) => {}
+    }
+}
+//TODO: stable可能和ic-structure 内存冲突  并且导致dfx无法正常启动和升级
+async fn push_pred()-> Result<(), String>{
+    Ok(())
+}
