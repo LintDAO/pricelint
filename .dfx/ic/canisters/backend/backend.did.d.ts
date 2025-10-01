623a3c0fbef13d1e75829ce408fea0e1135401a8
prj6 <prj6@qq.com>
2025-10-01 11:42:29 +0800

    Update did files

--- .dfx/ic/canisters/backend/backend.did.d.ts
+++ .dfx/ic/canisters/backend/backend.did.d.ts
@@ -2,6 +2,69 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
+export interface Account {
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
+export interface Approve {
+  'fee' : [] | [bigint],
+  'from' : Account,
+  'memo' : [] | [Uint8Array | number[]],
+  'created_at_time' : [] | [bigint],
+  'amount' : bigint,
+  'expected_allowance' : [] | [bigint],
+  'expires_at' : [] | [bigint],
+  'spender' : Account,
+}
+export interface ArchivedRange {
+  'callback' : [Principal, string],
+  'start' : bigint,
+  'length' : bigint,
+}
+export interface ArchivedRange_1 {
+  'callback' : [Principal, string],
+  'start' : bigint,
+  'length' : bigint,
+}
+export interface BlockRange { 'blocks' : Array<Value> }
+export interface Burn {
+  'from' : Account,
+  'memo' : [] | [Uint8Array | number[]],
+  'created_at_time' : [] | [bigint],
+  'amount' : bigint,
+  'spender' : [] | [Account],
+}
+export type DurationRange = { 'Microseconds' : null } |
+  { 'Minutes' : null } |
+  { 'Seconds' : null } |
+  { 'Days' : null } |
+  { 'Milliseconds' : null } |
+  { 'Hours' : null } |
+  { 'Nanoseconds' : null };
+export interface GetBlocksRequest { 'start' : bigint, 'length' : bigint }
+export interface GetBlocksResponse {
+  'certificate' : [] | [Uint8Array | number[]],
+  'first_index' : bigint,
+  'blocks' : Array<Value>,
+  'chain_length' : bigint,
+  'archived_blocks' : Array<ArchivedRange>,
+}
+export interface GetTransactionsResponse {
+  'first_index' : bigint,
+  'log_length' : bigint,
+  'transactions' : Array<Transaction>,
+  'archived_transactions' : Array<ArchivedRange_1>,
+}
+export interface ICRC2AllowanceResponse {
+  'allowance' : bigint,
+  'expires_at' : [] | [bigint],
+}
+export interface Mint {
+  'to' : Account,
+  'memo' : [] | [Uint8Array | number[]],
+  'created_at_time' : [] | [bigint],
+  'amount' : bigint,
+}
 export interface Pred {
   'up' : number,
   'staked' : number,
@@ -11,18 +74,24 @@ export interface Pred {
 export interface Predictor {
   'id' : string,
   'trend' : [] | [string],
-  'name' : string,
   'pred' : Pred,
+  'canister_id' : string,
+  'user_id' : string,
   'stake' : [number, number],
   'create_time' : bigint,
   'price' : number,
 }
+export interface PredictorResult {
+  'trend' : [] | [string],
+  'pred' : Pred,
+  'price' : [] | [number],
+}
 export interface PredictorView {
   'id' : string,
-  'now' : [] | [Predictor],
-  'next' : [] | [Predictor],
-  'last_1' : [] | [Predictor],
-  'last_2' : [] | [Predictor],
+  'now' : [] | [PredictorResult],
+  'next' : [] | [PredictorResult],
+  'last_1' : [] | [PredictorResult],
+  'last_2' : [] | [PredictorResult],
   'stake' : [number, number],
   'create_time' : bigint,
   'accuracy' : number,
@@ -37,17 +106,27 @@ export interface PriceData {
 }
 export type Result = { 'Ok' : WasmFile } |
   { 'Err' : string };
-export type Result_1 = { 'Ok' : string } |
+export type Result_1 = { 'Ok' : GetBlocksResponse } |
   { 'Err' : string };
-export type Result_2 = { 'Ok' : Uint8Array | number[] } |
+export type Result_10 = { 'Ok' : null } |
   { 'Err' : string };
-export type Result_3 = { 'Ok' : Array<WasmFile> } |
+export type Result_11 = { 'Ok' : User } |
   { 'Err' : string };
-export type Result_4 = { 'Ok' : PredictorView } |
+export type Result_2 = { 'Ok' : string } |
   { 'Err' : string };
-export type Result_5 = { 'Ok' : Array<Predictor> } |
+export type Result_3 = { 'Ok' : Array<Predictor> } |
   { 'Err' : string };
-export type Result_6 = { 'Ok' : User } |
+export type Result_4 = { 'Ok' : GetTransactionsResponse } |
+  { 'Err' : string };
+export type Result_5 = { 'Ok' : Array<WasmFile> } |
+  { 'Err' : string };
+export type Result_6 = { 'Ok' : bigint } |
+  { 'Err' : string };
+export type Result_7 = { 'Ok' : ICRC2AllowanceResponse } |
+  { 'Err' : string };
+export type Result_8 = { 'Ok' : Predictor } |
+  { 'Err' : string };
+export type Result_9 = { 'Ok' : PredictorView } |
   { 'Err' : string };
 export interface State {
   'bias' : [] | [Array<number>],
@@ -56,12 +135,46 @@ export interface State {
   'prices' : Array<PriceData>,
   'min_values' : Array<number>,
 }
+export interface Transaction {
+  'burn' : [] | [Burn],
+  'kind' : string,
+  'mint' : [] | [Mint],
+  'approve' : [] | [Approve],
+  'timestamp' : bigint,
+  'transfer' : [] | [Transfer],
+}
+export interface TransactionRange { 'transactions' : Array<Transaction> }
+export interface Transfer {
+  'to' : Account,
+  'fee' : [] | [bigint],
+  'from' : Account,
+  'memo' : [] | [Uint8Array | number[]],
+  'created_at_time' : [] | [bigint],
+  'amount' : bigint,
+  'spender' : [] | [Account],
+}
 export interface User {
   'id' : string,
   'owner' : Principal,
   'name' : string,
   'create_time' : bigint,
 }
+export type Value = { 'Int' : bigint } |
+  { 'Map' : Array<[string, Value]> } |
+  { 'Nat' : bigint } |
+  { 'Nat64' : bigint } |
+  { 'Blob' : Uint8Array | number[] } |
+  { 'Text' : string } |
+  { 'Array' : Vec };
+export type Vec = Array<
+  { 'Int' : bigint } |
+    { 'Map' : Array<[string, Value]> } |
+    { 'Nat' : bigint } |
+    { 'Nat64' : bigint } |
+    { 'Blob' : Uint8Array | number[] } |
+    { 'Text' : string } |
+    { 'Array' : Vec }
+>;
 export interface WasmFile {
   'wasm_version' : string,
   'wasm_bin' : Uint8Array | number[],
@@ -71,20 +184,41 @@ export interface _SERVICE {
   'add_price' : ActorMethod<[PriceData], undefined>,
   'delete_wasm' : ActorMethod<[string, string], Result>,
   'find_user_lists' : ActorMethod<[], Array<User>>,
-  'get_canister_info' : ActorMethod<[], Result_1>,
+  'get_blocks' : ActorMethod<[GetBlocksRequest], Result_1>,
+  'get_canister_info' : ActorMethod<[], Result_2>,
+  'get_predictor_vec' : ActorMethod<[], Result_3>,
+  'get_principal' : ActorMethod<[], Principal>,
   'get_state' : ActorMethod<[], State>,
-  'get_wasm' : ActorMethod<[string], Result_2>,
+  'get_transactions' : ActorMethod<[GetBlocksRequest], Result_4>,
   'get_wasm_bin' : ActorMethod<[string, string], Result>,
-  'get_wasm_lists' : ActorMethod<[], Result_3>,
-  'pred' : ActorMethod<[], Result_4>,
+  'get_wasm_lists' : ActorMethod<[], Result_5>,
+  'icrc1_balance_of' : ActorMethod<[], Result_6>,
+  'icrc1_transfer' : ActorMethod<
+    [Account, bigint, [] | [Uint8Array | number[]]],
+    Result_6
+  >,
+  'icrc2_allowance' : ActorMethod<[Account], Result_7>,
+  'icrc2_approve' : ActorMethod<[bigint], Result_2>,
+  'icrc2_transfer_from' : ActorMethod<
+    [Account, bigint, [] | [Uint8Array | number[]]],
+    Result_6
+  >,
+  'minting_or_burn' : ActorMethod<[Account, bigint], Result_6>,
   'predict' : ActorMethod<[], number>,
+  'push_user_pred' : ActorMethod<[Predictor], Result_8>,
   'refill_random_buffer' : ActorMethod<[number], undefined>,
-  'show_predictions' : ActorMethod<[], Result_5>,
+  'show_predictions' : ActorMethod<[], Result_9>,
+  'stake' : ActorMethod<[bigint, bigint], Result_10>,
+  'test_1' : ActorMethod<[DurationRange], [bigint, bigint]>,
   'train' : ActorMethod<[bigint], undefined>,
+  'unstake' : ActorMethod<[], Result_10>,
   'upload_json_file' : ActorMethod<[Uint8Array | number[]], undefined>,
-  'upload_wasm' : ActorMethod<[string, string], Result_2>,
-  'user_login' : ActorMethod<[], Result_6>,
-  'user_register' : ActorMethod<[], Result_6>,
+  'upload_wasm' : ActorMethod<
+    [string, string, Uint8Array | number[]],
+    Result_2
+  >,
+  'user_login' : ActorMethod<[], Result_11>,
+  'user_register' : ActorMethod<[], Result_11>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- .dfx/ic/canisters/backend/backend.did.js
+++ .dfx/ic/canisters/backend/backend.did.js
@@ -1,4 +1,6 @@
 export const idlFactory = ({ IDL }) => {
+  const Value = IDL.Rec();
+  const Vec = IDL.Rec();
   const PriceData = IDL.Record({
     'low' : IDL.Float32,
     'high' : IDL.Float32,
@@ -19,16 +21,49 @@ export const idlFactory = ({ IDL }) => {
     'name' : IDL.Text,
     'create_time' : IDL.Nat64,
   });
-  const Result_1 = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : IDL.Text });
-  const State = IDL.Record({
-    'bias' : IDL.Opt(IDL.Vec(IDL.Float32)),
-    'max_values' : IDL.Vec(IDL.Float32),
-    'weights' : IDL.Opt(IDL.Vec(IDL.Float32)),
-    'prices' : IDL.Vec(PriceData),
-    'min_values' : IDL.Vec(IDL.Float32),
+  const GetBlocksRequest = IDL.Record({
+    'start' : IDL.Nat,
+    'length' : IDL.Nat,
   });
-  const Result_2 = IDL.Variant({ 'Ok' : IDL.Vec(IDL.Nat8), 'Err' : IDL.Text });
-  const Result_3 = IDL.Variant({ 'Ok' : IDL.Vec(WasmFile), 'Err' : IDL.Text });
+  Vec.fill(
+    IDL.Vec(
+      IDL.Variant({
+        'Int' : IDL.Int,
+        'Map' : IDL.Vec(IDL.Tuple(IDL.Text, Value)),
+        'Nat' : IDL.Nat,
+        'Nat64' : IDL.Nat64,
+        'Blob' : IDL.Vec(IDL.Nat8),
+        'Text' : IDL.Text,
+        'Array' : Vec,
+      })
+    )
+  );
+  Value.fill(
+    IDL.Variant({
+      'Int' : IDL.Int,
+      'Map' : IDL.Vec(IDL.Tuple(IDL.Text, Value)),
+      'Nat' : IDL.Nat,
+      'Nat64' : IDL.Nat64,
+      'Blob' : IDL.Vec(IDL.Nat8),
+      'Text' : IDL.Text,
+      'Array' : Vec,
+    })
+  );
+  const BlockRange = IDL.Record({ 'blocks' : IDL.Vec(Value) });
+  const ArchivedRange = IDL.Record({
+    'callback' : IDL.Func([GetBlocksRequest], [BlockRange], ['query']),
+    'start' : IDL.Nat,
+    'length' : IDL.Nat,
+  });
+  const GetBlocksResponse = IDL.Record({
+    'certificate' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'first_index' : IDL.Nat,
+    'blocks' : IDL.Vec(Value),
+    'chain_length' : IDL.Nat64,
+    'archived_blocks' : IDL.Vec(ArchivedRange),
+  });
+  const Result_1 = IDL.Variant({ 'Ok' : GetBlocksResponse, 'Err' : IDL.Text });
+  const Result_2 = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : IDL.Text });
   const Pred = IDL.Record({
     'up' : IDL.Float64,
     'staked' : IDL.Float64,
@@ -38,43 +73,163 @@ export const idlFactory = ({ IDL }) => {
   const Predictor = IDL.Record({
     'id' : IDL.Text,
     'trend' : IDL.Opt(IDL.Text),
-    'name' : IDL.Text,
     'pred' : Pred,
+    'canister_id' : IDL.Text,
+    'user_id' : IDL.Text,
     'stake' : IDL.Tuple(IDL.Float64, IDL.Float64),
     'create_time' : IDL.Nat64,
     'price' : IDL.Float64,
   });
+  const Result_3 = IDL.Variant({ 'Ok' : IDL.Vec(Predictor), 'Err' : IDL.Text });
+  const State = IDL.Record({
+    'bias' : IDL.Opt(IDL.Vec(IDL.Float32)),
+    'max_values' : IDL.Vec(IDL.Float32),
+    'weights' : IDL.Opt(IDL.Vec(IDL.Float32)),
+    'prices' : IDL.Vec(PriceData),
+    'min_values' : IDL.Vec(IDL.Float32),
+  });
+  const Account = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const Burn = IDL.Record({
+    'from' : Account,
+    'memo' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'created_at_time' : IDL.Opt(IDL.Nat64),
+    'amount' : IDL.Nat,
+    'spender' : IDL.Opt(Account),
+  });
+  const Mint = IDL.Record({
+    'to' : Account,
+    'memo' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'created_at_time' : IDL.Opt(IDL.Nat64),
+    'amount' : IDL.Nat,
+  });
+  const Approve = IDL.Record({
+    'fee' : IDL.Opt(IDL.Nat),
+    'from' : Account,
+    'memo' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'created_at_time' : IDL.Opt(IDL.Nat64),
+    'amount' : IDL.Nat,
+    'expected_allowance' : IDL.Opt(IDL.Nat),
+    'expires_at' : IDL.Opt(IDL.Nat64),
+    'spender' : Account,
+  });
+  const Transfer = IDL.Record({
+    'to' : Account,
+    'fee' : IDL.Opt(IDL.Nat),
+    'from' : Account,
+    'memo' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'created_at_time' : IDL.Opt(IDL.Nat64),
+    'amount' : IDL.Nat,
+    'spender' : IDL.Opt(Account),
+  });
+  const Transaction = IDL.Record({
+    'burn' : IDL.Opt(Burn),
+    'kind' : IDL.Text,
+    'mint' : IDL.Opt(Mint),
+    'approve' : IDL.Opt(Approve),
+    'timestamp' : IDL.Nat64,
+    'transfer' : IDL.Opt(Transfer),
+  });
+  const TransactionRange = IDL.Record({
+    'transactions' : IDL.Vec(Transaction),
+  });
+  const ArchivedRange_1 = IDL.Record({
+    'callback' : IDL.Func([GetBlocksRequest], [TransactionRange], ['query']),
+    'start' : IDL.Nat,
+    'length' : IDL.Nat,
+  });
+  const GetTransactionsResponse = IDL.Record({
+    'first_index' : IDL.Nat,
+    'log_length' : IDL.Nat,
+    'transactions' : IDL.Vec(Transaction),
+    'archived_transactions' : IDL.Vec(ArchivedRange_1),
+  });
+  const Result_4 = IDL.Variant({
+    'Ok' : GetTransactionsResponse,
+    'Err' : IDL.Text,
+  });
+  const Result_5 = IDL.Variant({ 'Ok' : IDL.Vec(WasmFile), 'Err' : IDL.Text });
+  const Result_6 = IDL.Variant({ 'Ok' : IDL.Nat, 'Err' : IDL.Text });
+  const ICRC2AllowanceResponse = IDL.Record({
+    'allowance' : IDL.Nat,
+    'expires_at' : IDL.Opt(IDL.Nat64),
+  });
+  const Result_7 = IDL.Variant({
+    'Ok' : ICRC2AllowanceResponse,
+    'Err' : IDL.Text,
+  });
+  const Result_8 = IDL.Variant({ 'Ok' : Predictor, 'Err' : IDL.Text });
+  const PredictorResult = IDL.Record({
+    'trend' : IDL.Opt(IDL.Text),
+    'pred' : Pred,
+    'price' : IDL.Opt(IDL.Float64),
+  });
   const PredictorView = IDL.Record({
     'id' : IDL.Text,
-    'now' : IDL.Opt(Predictor),
-    'next' : IDL.Opt(Predictor),
-    'last_1' : IDL.Opt(Predictor),
-    'last_2' : IDL.Opt(Predictor),
+    'now' : IDL.Opt(PredictorResult),
+    'next' : IDL.Opt(PredictorResult),
+    'last_1' : IDL.Opt(PredictorResult),
+    'last_2' : IDL.Opt(PredictorResult),
     'stake' : IDL.Tuple(IDL.Float64, IDL.Float64),
     'create_time' : IDL.Nat64,
     'accuracy' : IDL.Float64,
   });
-  const Result_4 = IDL.Variant({ 'Ok' : PredictorView, 'Err' : IDL.Text });
-  const Result_5 = IDL.Variant({ 'Ok' : IDL.Vec(Predictor), 'Err' : IDL.Text });
-  const Result_6 = IDL.Variant({ 'Ok' : User, 'Err' : IDL.Text });
+  const Result_9 = IDL.Variant({ 'Ok' : PredictorView, 'Err' : IDL.Text });
+  const Result_10 = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text });
+  const DurationRange = IDL.Variant({
+    'Microseconds' : IDL.Null,
+    'Minutes' : IDL.Null,
+    'Seconds' : IDL.Null,
+    'Days' : IDL.Null,
+    'Milliseconds' : IDL.Null,
+    'Hours' : IDL.Null,
+    'Nanoseconds' : IDL.Null,
+  });
+  const Result_11 = IDL.Variant({ 'Ok' : User, 'Err' : IDL.Text });
   return IDL.Service({
     'add_price' : IDL.Func([PriceData], [], []),
     'delete_wasm' : IDL.Func([IDL.Text, IDL.Text], [Result], []),
     'find_user_lists' : IDL.Func([], [IDL.Vec(User)], ['query']),
-    'get_canister_info' : IDL.Func([], [Result_1], []),
+    'get_blocks' : IDL.Func([GetBlocksRequest], [Result_1], []),
+    'get_canister_info' : IDL.Func([], [Result_2], []),
+    'get_predictor_vec' : IDL.Func([], [Result_3], ['query']),
+    'get_principal' : IDL.Func([], [IDL.Principal], ['query']),
     'get_state' : IDL.Func([], [State], ['query']),
-    'get_wasm' : IDL.Func([IDL.Text], [Result_2], ['query']),
+    'get_transactions' : IDL.Func([GetBlocksRequest], [Result_4], []),
     'get_wasm_bin' : IDL.Func([IDL.Text, IDL.Text], [Result], ['query']),
-    'get_wasm_lists' : IDL.Func([], [Result_3], ['query']),
-    'pred' : IDL.Func([], [Result_4], ['query']),
+    'get_wasm_lists' : IDL.Func([], [Result_5], ['query']),
+    'icrc1_balance_of' : IDL.Func([], [Result_6], []),
+    'icrc1_transfer' : IDL.Func(
+        [Account, IDL.Nat, IDL.Opt(IDL.Vec(IDL.Nat8))],
+        [Result_6],
+        [],
+      ),
+    'icrc2_allowance' : IDL.Func([Account], [Result_7], []),
+    'icrc2_approve' : IDL.Func([IDL.Nat], [Result_2], []),
+    'icrc2_transfer_from' : IDL.Func(
+        [Account, IDL.Nat, IDL.Opt(IDL.Vec(IDL.Nat8))],
+        [Result_6],
+        [],
+      ),
+    'minting_or_burn' : IDL.Func([Account, IDL.Nat], [Result_6], []),
     'predict' : IDL.Func([], [IDL.Float32], ['query']),
+    'push_user_pred' : IDL.Func([Predictor], [Result_8], []),
     'refill_random_buffer' : IDL.Func([IDL.Nat32], [], []),
-    'show_predictions' : IDL.Func([], [Result_5], ['query']),
+    'show_predictions' : IDL.Func([], [Result_9], ['query']),
+    'stake' : IDL.Func([IDL.Nat, IDL.Nat64], [Result_10], []),
+    'test_1' : IDL.Func([DurationRange], [IDL.Nat64, IDL.Nat64], ['query']),
     'train' : IDL.Func([IDL.Nat64], [], []),
+    'unstake' : IDL.Func([], [Result_10], []),
     'upload_json_file' : IDL.Func([IDL.Vec(IDL.Nat8)], [], []),
-    'upload_wasm' : IDL.Func([IDL.Text, IDL.Text], [Result_2], []),
-    'user_login' : IDL.Func([], [Result_6], ['query']),
-    'user_register' : IDL.Func([], [Result_6], []),
+    'upload_wasm' : IDL.Func(
+        [IDL.Text, IDL.Text, IDL.Vec(IDL.Nat8)],
+        [Result_2],
+        [],
+      ),
+    'user_login' : IDL.Func([], [Result_11], ['query']),
+    'user_register' : IDL.Func([], [Result_11], []),
   });
 };
 export const init = ({ IDL }) => { return []; };
